6dd624ac89f448c181817df019438bc6fd66e8ba
EC2 Default User <ec2-user@ip-172-31-39-58.us-east-2.compute.internal>
2018-05-02 06:45:33 +0000

    css makeup

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,7 +7,60 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 
+User.create!(first_name:  "Henry",
+             last_name: "Lin",
+             uin: "123456789",
+             phone: "1234567890",
+             email: "henrysurface@gmail.com",
+             password:              "123456",
+             password_confirmation: "123456",
+             email_confirmed: true)
+             
+User.create!(first_name:  "Henry2",
+             last_name: "Lin",
+             uin: "123456790",
+             phone: "1234567891",
+             email: "harrysurfacetw@gmail.com",
+             password:              "123456",
+             password_confirmation: "123456",
+             email_confirmed: true)
+             
+User.create!(first_name:  "Henry3",
+             last_name: "Lin",
+             uin: "123456791",
+             phone: "1234567892",
+             email: "henrysurface@tmu.com",
+             password:              "123456",
+             password_confirmation: "123456",
+             email_confirmed: true)
 
+
+Admin.create!(name:  "Henry",
+             email: "henrysurface@gmail.com",
+             password:              "123456",
+             password_confirmation: "123456",
+             code: "jzNvvyqZ",
+             superadmin: true,
+             email_confirmed: true
+             )
+
+
+100.times do |n|
+  first_name  = Faker::Name.first_name
+  last_name  = Faker::Name.last_name
+  email = Faker::Internet.email
+  uin = Faker::Number.number(9)
+  phone = Faker::Number.number(10)
+  password = "password"
+  User.create!(first_name:  first_name,
+               last_name: last_name,
+               email: email,
+               uin: uin,
+               phone: phone,
+               password:              password,
+               password_confirmation: password,
+               email_confirmed: true)
+end
 
 100.times do |n|
   appid  = Faker::GameOfThrones.character
